c9739a67029f3c856f50bfa8f4036010d81b5b53
sacquin <sacquin@ibpc.fr>
2025-10-30 15:30:26 +0100

    Add files via upload

---
+++ preparation-simulation-files/template/step5_production12.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#MSUB -r prod12_CUSTOM
+#MSUB -q rome
+#MSUB -A gen13385
+#MSUB -m work
+#MSUB -n 512
+#MSUB -c 4
+#MSUB -T 86400
+
+module purge
+module load intel/20.0.0 mpi/openmpi/ namd/2.13
+
+fin=step5_production12.inp
+fout=step5_production12.out
+
+if ccc_mprun namd2 ++ppn 2 $fin > $fout ; then
+	ccc_msub step5_production13.sh
+else
+	exit
+fi
+done
