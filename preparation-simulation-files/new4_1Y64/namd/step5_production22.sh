5c5aab917fc7890db72471c3b0255e92a2ca6802
sacquin <sacquin@ibpc.fr>
2025-10-30 16:17:09 +0100

    Add files via upload

---
+++ preparation-simulation-files/new4_1Y64/namd/step5_production22.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#MSUB -r prod22_1Y64
+#MSUB -q rome
+#MSUB -A gen16135
+#MSUB -m scratch
+#MSUB -N 8
+#MSUB -n 512
+#MSUB -x
+#MSUB -T 86400
+
+module purge
+#module load intel/20.0.0 mpi/openmpi/ namd/2.13
+module load gnu/8 mpi/openmpi/4 flavor/namd/fullMPI namd/2.14
+
+
+fin=step5_production22.inp
+fout=step5_production22.out
+
+if ccc_mprun namd2 ++ppn 2 $fin > $fout ; then
+	ccc_msub step5_production23.sh
+else
+	exit
+fi
+done
---
+++ preparation-simulation-files/new4_1Y64/namd/test_equilibration.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#MSUB -r eq_1Y64
+#MSUB -q rome
+#MSUB -A gen13385
+#MSUB -m work
+#MSUB -n 512
+#MSUB -T 86400
+#MSUB -c 4
+
+module purge
+module load intel/20.0.0 mpi/openmpi/ namd/2.13
+
+ccc_mprun namd2 ++ppn 2 step4_equilibration.inp >& step4_equilibration.log
